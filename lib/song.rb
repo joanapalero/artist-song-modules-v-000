0ea86fabf8247bab105106d752005bea7878e98e
Joana Palero <joana.palero24@gmail.com>
2018-07-08 21:45:47 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,9 +12,9 @@ class Song
 
   @@songs = []
 
-  # def self.find_by_name(name)
-  #   @@songs.detect{|a| a.name == name}
-  # end
+  def self.find_by_name(name)
+    @@songs.detect{|a| a.name == name}
+  end
 
   def artist=(artist)
     @artist = artist
